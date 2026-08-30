b8329a8e9f361202b1965b33ecc7f11a4b2ef42e
mujii88 <mujtabakhan1036k@gmail.com>
2026-08-30 23:17:14 +0500

    initital template for Evaluate

--- backend/engine/evaluator.go
+++ backend/engine/evaluator.go
@@ -1,1 +1,17 @@
 package engine
+
+
+
+type Result struct {
+	Status  string `json:"status"`
+	Output string `json:"output"`
+	Error string `json:"error,omitempty"`
+
+}
+
+func Evaluate(code string , testbench string , workerName string) Result {
+
+
+ return Result{Status : "First Handshake", Output: "output", Error: "Not till Now"}
+
+}
